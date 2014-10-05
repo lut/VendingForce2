fb935004d6a3fb91e7006d795c0d9086032d347d
Lut Garcia <lutgardo21@gmail.com>
2014-10-05 10:21:32 -0500

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+VendingForce2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VendingForce2::Application.config.secret_token = '6583911562fdfe984b6ea5e34c41b632903d3d1c72ba99d23846b4b0404ee259aece882c01018b381503b18644a1e3729275f5caf0926f7f10c6f92df608959f'
